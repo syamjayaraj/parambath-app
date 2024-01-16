bb1e9c2b417f6ca2d5f8c04a3fe8d1868fc2c20a
Syamlal CM <syamjayaraj@gmail.com>
2024-01-16 19:58:52 +0530

    design changes, build fixes

--- tabs/components/main-event/index.tsx
+++ tabs/components/main-event/index.tsx
@@ -732,13 +732,14 @@ const styles = StyleSheet.create({
     // borderColor: "#969696",
     borderRadius: 5,
     padding: 8,
-    paddingTop: 4,
-    paddingBottom: 4,
+    paddingTop: 2,
+    paddingRight: 12,
+    paddingBottom: 6,
     backgroundColor: "#1c1b29",
   },
   shareIcon: {
     fontSize: 20,
-    marginRight: 10,
+    marginRight: 5,
     color: "white",
   },
   shareText: {
