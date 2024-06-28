476afc646322d23890877062a7da9f14a8587e86
Syamlal CM <syamjayaraj@gmail.com>
2024-06-28 17:12:07 +0530

    config.example added

---
+++ config.example.js
@@ -0,0 +1,6 @@
+let apiUrl = "https://api-v5-parambath.onnich.com/api/";
+let apiDomain = "https://api-v5-parambath.onnich.com";
+const pageSize = 50;
+const categorySize = 100;
+
+export { apiUrl, apiDomain, pageSize, categorySize };
