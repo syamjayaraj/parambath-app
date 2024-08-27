4302ed030f41afb104ced67095f8f6f2ef9429fd
Syamlal CM <syamjayaraj@gmail.com>
2024-08-27 15:28:07 +0530

    upgraded to SDK 51

--- utils/call-to-number.ts
+++ utils/call-to-number.ts
@@ -1,12 +1,19 @@
-import call from "react-native-phone-call";
+import { Alert, Linking } from "react-native";
 
 const callToTheNumber = async (phoneNumber: any, prompt?: boolean) => {
   try {
-    let callArgs = {
-      number: phoneNumber,
-      prompt: prompt ? prompt : false,
-    };
-    await call(callArgs);
+    Linking.canOpenURL(`tel:${phoneNumber}`)
+      .then((supported) => {
+        if (supported) {
+          Linking.openURL(`tel:${phoneNumber}`);
+        } else {
+          Alert.alert("Error", "Phone call not supported on this device.");
+        }
+      })
+      .catch((err) => {
+        Alert.alert("Error", "An error occurred while trying to make a call.");
+        console.error(err);
+      });
   } catch (err: any) {}
 };
 
